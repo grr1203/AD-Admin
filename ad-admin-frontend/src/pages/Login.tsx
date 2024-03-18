90144cdfb61a64e4ed566e8e75d07fabb331f8d9
reconlabs-hyokeun-logan <logan@reconlabs.kr>
2024-03-18 13:51:05 +0900

    Login Page UI 구현

--- ad-admin-frontend/src/pages/Login.tsx
+++ ad-admin-frontend/src/pages/Login.tsx
@@ -1,5 +1,40 @@
-function Login() {
-  return <>login</>;
-}
+import React, { useState } from 'react';
+
+const Login: React.FC = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [autoLogin, setAutoLogin] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('id:', id, 'Password:', password, 'autoLogin:', autoLogin);
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <div className="logo">AD</div>
+        <div>
+          <input type="id" value={id} onChange={(e) => setId(e.target.value)} placeholder="ID" required />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div className="form-footer">
+          <input type="checkbox" id="loginCheckbox" onChange={() => setAutoLogin(!autoLogin)} />
+          <label className="login-checkbox" htmlFor="loginCheckbox"></label>
+          <label htmlFor="loginCheckbox">자동 로그인</label>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
