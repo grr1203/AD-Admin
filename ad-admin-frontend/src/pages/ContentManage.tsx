8d50c97161d95b3acd8f100d5793799da54a5538
grr1203 <gyrms9412@naver.com>
2024-03-22 18:01:36 +0900

    화면 비율별 UI 구현 및 분기 처리

--- ad-admin-frontend/src/pages/ContentManage.tsx
+++ ad-admin-frontend/src/pages/ContentManage.tsx
@@ -29,6 +29,7 @@ import { HiDotsHorizontal } from 'react-icons/hi';
 function ContentManage() {
   const [deviceList, setDeviceList] = useState([]);
   const [currentDevice, setCurrentDevice] = useState<Device | null>(null);
+  const [scrrenDirection, setScreenDirection] = useState('height'); // width / height
   const [contentList, setContentList] = useState<string[]>([]);
   const [file, setFile] = useState<File | null>(null);
   const [openFailDialog, setOpenFailDialog] = useState('');
@@ -46,14 +47,19 @@ function ContentManage() {
   }, []);
 
   useEffect(() => {
+    if (!currentDevice) return;
+
+    const screenSize = JSON.parse(currentDevice.screenSize);
+    setScreenDirection(screenSize.width > screenSize.height ? 'width' : 'height');
+
+    setContentList([]);
     (async () => await getContentList())();
   }, [currentDevice]);
 
   // 장치의 컨텐츠 목록 조회
   const getContentList = async () => {
-    if (!currentDevice) return; // default
     try {
-      const res = await fetch(`http://${currentDevice.ip}/ad/list`);
+      const res = await fetch(`http://${currentDevice!.ip}/ad/list`);
       const data = await res.json();
       console.log('get ad list response:', data);
       setContentList(data.adList);
@@ -119,29 +125,33 @@ function ContentManage() {
   };
 
   return (
-    <div className="hidden h-[calc(100vh_-_4rem)] overflow-hidden flex-col gap-8 md:flex">
-      <div className="space-y-2">
+    <div className="hidden h-[100vh] overflow-hidden flex-col gap-8 md:flex">
+      <div className="space-y-2 px-8 pt-8">
         <h1 className="text-4xl font-semibold tracking-tight pb-1">Contents</h1>
-        <p className="text-muted-foreground mb-6">컨텐츠 관리</p>
+        <p className="text-muted-foreground">컨텐츠 관리</p>
       </div>
-      <div className="flex flex-grow overflow-hidden justify-between gap-8">
-        <Card className="flex flex-col gap-3 h-full flex-[3]">
+      <div className="h-full flex flex-grow overflow-hidden justify-between gap-8 px-8 pb-8">
+        <Card className="flex flex-col gap-1 flex-[3] shadow">
           <CardHeader>
             <CardTitle className="font-normal">장치 목록</CardTitle>
           </CardHeader>
           <CardContent className="flex-grow overflow-auto">
             <ScrollArea className="h-full rounded-md border">
               <div>
-                <h4 className="mb-4 text-sm font-medium leading-none tracking-wide px-4 pt-4">등록명 / IP</h4>
+                <h4 className="text-sm font-medium leading-none tracking-wide px-4 py-4 bg-[#F9F9F9] text-[#888888] flex justify-between">
+                  <div className="flex-[2]">등록명</div>
+                  <div className="flex-[1]">IP</div>
+                </h4>
+                <Separator />
                 {deviceList.map((device: Device) => (
                   <div
                     key={device.idx}
-                    className="px-4 pt-3 pb-2 hover:bg-[#F5F6F9] cursor-pointer"
+                    className="pt-3 hover:bg-[#F5F6F9] cursor-pointer"
                     onClick={() => setCurrentDevice(device)}
                   >
-                    <div className="flex justify-between pb-1">
-                      <div className="text-sm">{device.name}</div>
-                      <div className="text-sm">{device.ip}</div>
+                    <div className="flex justify-between px-4 pb-3">
+                      <div className="flex-[2] text-sm">{device.name}</div>
+                      <div className="flex-[1] text-sm">{device.ip}</div>
                     </div>
                     <Separator />
                   </div>
@@ -150,33 +160,38 @@ function ContentManage() {
             </ScrollArea>
           </CardContent>
         </Card>
-        <Card className="h-full flex-[6] flex flex-col gap-3">
+        <Card className="h-full flex-[6] flex flex-col gap-1 shadow">
           <CardHeader>
             <CardTitle className="font-normal">{currentDevice?.name}</CardTitle>
           </CardHeader>
-          <CardContent className="flex h-full justify-center items-start">
-            {currentDevice ? (
-              <div className="w-full h-full flex justify-center items-center gap-10 pr-10">
-                <div className="flex-[6] h-full flex justify-center">
-                  <iframe
-                    src={`http://${currentDevice?.ip}`}
-                    title="Client"
-                    className={`h-[95%] aspect-[1080/1920] relative rounded-sm`}
-                    // className={`w-full aspect-[${calculateScreenRatio(currentDevice?.screenSize)}] relative rounded-sm`} // translate-y-[50%]
-                  ></iframe>
-                </div>
-                <div className="flex-[5] h-full">
-                  <ScrollArea className={`rounded-md border h-3/4 overflow-auto`}>
-                    <div>
-                      <h4 className="mb-4 text-sm font-medium leading-none tracking-wide px-4 pt-4">컨텐츠 목록</h4>
+          {currentDevice ? (
+            scrrenDirection === 'height' ? (
+              <CardContent className="flex h-full justify-center items-start">
+                <div className="w-full h-full flex justify-center items-center gap-10 pr-10">
+                  <div className="flex-[6] h-full flex justify-center">
+                    <iframe
+                      src={`http://${currentDevice?.ip}`}
+                      title="Client"
+                      className={`h-[95%] aspect-[9/16] relative rounded-sm`}
+                    ></iframe>
+                  </div>
+                  <div className="flex-[5] h-full flex-col justify-between">
+                    <ScrollArea className={`h-3/4 rounded-md border overflow-auto`}>
+                      <h4 className="text-sm font-medium leading-none tracking-wide px-4 py-4 bg-[#F9F9F9] text-[#888888]">
+                        컨텐츠 목록
+                      </h4>
+                      <Separator />
                       {contentList.length > 0 ? (
                         contentList.map((content, index) => (
-                          <div key={content} className="px-4 pt-3 pb-1 hover:bg-[#F5F6F9]" onClick={() => {}}>
-                            <div className="flex justify-between pb-1">
+                          <div key={content} className="pt-3 hover:bg-[#F5F6F9]" onClick={() => {}}>
+                            <div className="flex justify-between pb-1 px-4 ">
                               <div className="text-sm">{content}</div>
                               <DropdownMenu>
                                 <DropdownMenuTrigger asChild>
-                                  <Button variant="ghost" className="flex h-6 w-6 p-0 data-[state=open]:bg-muted">
+                                  <Button
+                                    variant="ghost"
+                                    className="flex focus-visible:ring-0 focus-visible:ring-offset-0 h-6 w-6 p-0 data-[state=open]:bg-muted"
+                                  >
                                     <HiDotsHorizontal className="h-4 w-4 text-[#555555]" />
                                   </Button>
                                 </DropdownMenuTrigger>
@@ -206,10 +221,8 @@ function ContentManage() {
                           </div>
                         </div>
                       )}
-                    </div>
-                  </ScrollArea>
-                  <div className="mt-5">
-                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                    </ScrollArea>
+                    <div className="grid w-full items-center gap-3 mt-5">
                       <Input
                         id="picture"
                         type="file"
@@ -220,11 +233,79 @@ function ContentManage() {
                     </div>
                   </div>
                 </div>
-              </div>
+              </CardContent>
             ) : (
-              <div className="h-full flex justify-center items-center text-[#999999]">장치를 선택해주세요</div>
-            )}
-          </CardContent>
+              <CardContent className="flex-grow overflow-auto">
+                <div className="w-full flex-col flex-grow justify-center items-center gap-10">
+                  <div className="flex mb-6 items-end">
+                    <iframe
+                      src={`http://${currentDevice?.ip}`}
+                      title="Client"
+                      className={`flex-[3] aspect-[16/9] relative rounded-sm pr-6`}
+                    ></iframe>
+                    <div className="flex-[1] grid">
+                      <Input
+                        className="mb-3"
+                        id="picture"
+                        type="file"
+                        accept="video/*,image/*"
+                        onChange={(event) => event.target.files && setFile(event.target.files[0])}
+                      />
+                      <Button onClick={handleUpload}>업로드</Button>
+                    </div>
+                  </div>
+                  <ScrollArea className={`h-full rounded-md border overflow-auto`}>
+                    <h4 className="text-sm font-medium leading-none tracking-wide px-4 py-4 bg-[#F9F9F9] text-[#888888]">
+                      컨텐츠 목록
+                    </h4>
+                    <Separator />
+                    {contentList.length > 0 ? (
+                      contentList.map((content, index) => (
+                        <div key={content} className="pt-3 hover:bg-[#F5F6F9]" onClick={() => {}}>
+                          <div className="flex justify-between pb-2 px-4">
+                            <div className="text-sm">{content}</div>
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button
+                                  variant="ghost"
+                                  className="flex focus-visible:ring-0 focus-visible:ring-offset-0 h-6 w-6 p-0 data-[state=open]:bg-muted"
+                                >
+                                  <HiDotsHorizontal className="h-4 w-4 text-[#555555]" />
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent
+                                align="end"
+                                className="w-[160px]"
+                                onClick={() => setCurrentContentIndex(index)}
+                              >
+                                <DropdownMenuItem onClick={() => setOpenChangeMainContentDialog(true)}>
+                                  적용
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={() => setOpenDeleteContentDialog(true)}>
+                                  삭제
+                                  <DropdownMenuShortcut>⌫</DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          </div>
+                          <Separator />
+                        </div>
+                      ))
+                    ) : (
+                      <div className="px-4 py-2 hover:bg-[#F5F6F9] cursor-pointer">
+                        <div className="flex justify-between">
+                          <div className="text-sm text-[#999999]">No Contents</div>
+                        </div>
+                      </div>
+                    )}
+                  </ScrollArea>
+                </div>
+              </CardContent>
+            )
+          ) : (
+            <div className="h-full flex justify-center items-center text-[#999999]">장치를 선택해주세요</div>
+          )}
         </Card>
         <Dialog open={openFailDialog !== ''} onOpenChange={(open) => !open && setOpenFailDialog('')}>
           <DialogContent className="sm:max-w-md">
@@ -281,14 +362,5 @@ function ContentManage() {
     </div>
   );
 }
-
-const calculateScreenRatio = (screenSize?: string) => {
-  if (!screenSize) return '16/9';
-
-  const { width, height } = JSON.parse(screenSize);
-  console.log('`${width / height}`:', `${width} / ${height}`);
-
-  return `${width}/${height}`;
-};
 
 export default ContentManage;
