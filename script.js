4fdb021c183d96a4b9071f99a95e5d1fb58be63e
grr1203 <gyrms9412@naver.com>
2024-03-25 21:28:54 +0900

    Build & 실행 Script 구현

---
+++ script.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process');
+
+exec('forever start build/server.js', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`server 실행 중 오류 발생: ${error}`);
+    return;
+  }
+  console.log(`server 실행 결과: ${stdout}`);
+  console.error(`server 실행 오류: ${stderr}`);
+
+  setTimeout(() => exec(`open 'http://127.0.0.1:4001'`), 5000);
+});
